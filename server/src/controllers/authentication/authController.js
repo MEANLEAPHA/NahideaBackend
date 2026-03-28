c8272f0f6ed11856710d5564c595234f3afd1542
MEANLEAPHA <161608522+MEANLEAPHA@users.noreply.github.com>
2026-03-28 23:26:38 +0700

    should not use trim to chaining the number

--- server/src/controllers/authentication/authController.js
+++ server/src/controllers/authentication/authController.js
@@ -183,7 +183,7 @@ const verifyEmail = async (req, res) => {
       return res.status(400).json({ message: "Invalid PIN format" });
     }
 
-    const pinTrimmed = Number(pin).trim();
+    const pinTrimmed = Number(pin.trim());
 
     const [users] = await pool.query(
       "SELECT * FROM users WHERE email = ? AND pin_code = ?",
