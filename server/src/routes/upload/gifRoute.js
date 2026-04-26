0413f6db43bc34b263ba3696c7e9340f94308a2a
MEANLEAPHA <161608522+MEANLEAPHA@users.noreply.github.com>
2026-04-26 13:41:57 +0700

    gif on cloudinary

--- server/src/routes/upload/gifRoute.js
+++ server/src/routes/upload/gifRoute.js
@@ -1,11 +1,23 @@
+// const express = require("express");
+// const multer = require("multer");
+// const { uploadGif, getGifs, searchGif } = require("../../controllers/upload/gifController");
+
+
+// const router = express.Router();
+// const upload = multer({ storage: multer.memoryStorage() });
+// const { protect } = require("../../middleware/authMiddleware");
+
+// router.post("/upload", upload.single("gif"), uploadGif);
+// router.get("/", getGifs);
+// router.get("/search", searchGif);
+
+// module.exports = router;
 const express = require("express");
 const multer = require("multer");
 const { uploadGif, getGifs, searchGif } = require("../../controllers/upload/gifController");
 
-
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
-const { protect } = require("../../middleware/authMiddleware");
 
 router.post("/upload", upload.single("gif"), uploadGif);
 router.get("/", getGifs);
