7eec66d5752bdb1727e130b095014dba0f7d9962
MEANLEAPHA <161608522+MEANLEAPHA@users.noreply.github.com>
2026-05-13 14:48:56 +0700

    ranking notification more to go searching blah blah

--- server/src/routes/authentication/authRoutes.js
+++ server/src/routes/authentication/authRoutes.js
@@ -36,6 +36,6 @@ router.put(
 );
 
 router.get("/me", protect, getUserInfo);
-router.get("/get-user-info/:userId", protect, getUserInfoById);
+router.get("/get-user-info/:userId", getUserInfoById);
 
 module.exports = router;
