43cac7811f3979f965b421e24a0235193a93f696
MEANLEAPHA <161608522+MEANLEAPHA@users.noreply.github.com>
2026-05-16 14:26:52 +0700

    wrong require

--- server/src/routes/report/reportPostRoute.js
+++ server/src/routes/report/reportPostRoute.js
@@ -1,8 +1,7 @@
-const express = "express";
+const express = require("express");
+const router = express.Router();
 const  { createReport } = require("../../controllers/report/reportPostController.js");
 const {protect} = require("../../middleware/authMiddleware");
-
-const router = express.Router();
 
 router.post("/report-post", protect, createReport);
 
