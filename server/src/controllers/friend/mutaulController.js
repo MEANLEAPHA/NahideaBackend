701823743ba8411288081cac2bb6ae759abb9439
MEANLEAPHA <161608522+MEANLEAPHA@users.noreply.github.com>
2026-05-29 16:58:54 +0700

    gg

--- server/src/controllers/friend/mutaulController.js
+++ server/src/controllers/friend/mutaulController.js
@@ -4,22 +4,20 @@ const getMutuals= async (req, res) => {
   try {
     const userId = req.user.userId;
 
-    const result = await pool.query(
-    `
-    SELECT u.id, u.username, u.avatar_url
-    FROM users u
-    WHERE u.id IN (
-        SELECT f1.following_id
-        FROM follows f1
-        JOIN follows f2
-        ON f1.following_id = f2.follower_id
-        WHERE f1.follower_id = ?
-        AND f2.following_id = f1.follower_id
-    )
-    LIMIT 4
-    `,
-    [userId]
-    );
+    const [result] = await pool.query(
+  `
+  SELECT u.id, u.username, u.avatar_url
+  FROM users u
+  JOIN follows f1 ON u.id = f1.following_id
+  JOIN follows f2 ON f1.following_id = f2.follower_id
+  WHERE f1.follower_id = ?
+    AND f2.following_id = f1.follower_id
+  ORDER BY f1.created_at DESC
+  LIMIT 4
+  `,
+  [userId]
+);
+
 
 
     res.status(200).json({
