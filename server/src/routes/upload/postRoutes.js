98a82c4a81be446cd9855e77a5dea4ec90af0b44
MEANLEAPHA <161608522+MEANLEAPHA@users.noreply.github.com>
2026-06-05 22:46:54 +0700

    still test

--- server/src/routes/upload/postRoutes.js
+++ server/src/routes/upload/postRoutes.js
@@ -10,7 +10,6 @@ const {
   deletePost,
   likePost,
   favoritePost,
-  // getPostsByLike
 
 } = require("../../controllers/upload/postController");
 const {
@@ -53,7 +52,7 @@ router.post("/create-posts", protect,
     favoritePost
   );
 
-// router.get("/posts/likes", protect, getPostsByLike);
+
 // router.post("/:id/comments", protect, addComment);
 // router.post("/:id/vote", protect, vote); 
 // router.patch("/:id/solve", protect, markSolved);
