534bb1a7323b7ce9d45ef2c2ce6128dae976380c
MEANLEAPHA <161608522+MEANLEAPHA@users.noreply.github.com>
2026-07-18 09:40:36 +0700

    increase db liit to 20

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -3,19 +3,18 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   host: process.env.PG_DB_HOST,
-  user: process.env.PG_DB_USER,                          
-  password: process.env.PG_DB_PASSWORD,       
-  database: process.env.PG_DB_NAME,                        
-  port:  process.env.PG_DB_PORT,                                
-  max: process.env.PG_DB_LIMIT,                                  
-  idleTimeoutMillis: process.env.PG_IDLETIMEOUTMILLIS,
-  ssl: { rejectUnauthorized: false }  
+  user: process.env.PG_DB_USER,
+  password: process.env.PG_DB_PASSWORD,
+  database: process.env.PG_DB_NAME,
+  port: parseInt(process.env.PG_DB_PORT, 10),
+  max: parseInt(process.env.PG_DB_LIMIT, 10),
+  idleTimeoutMillis: parseInt(process.env.PG_IDLETIMEOUTMILLIS, 10),
+  connectionTimeoutMillis: 5000,
+  ssl: { rejectUnauthorized: false }
 });
-
 
 pool.on('error', (err) => {
   console.error("❌ Unexpected error on DB_Connection:", err);
 });
 
 module.exports = pool;
-
